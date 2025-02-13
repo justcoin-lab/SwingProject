e91b307d0e6d0e37cb0fa79c693b14e4db344723
hello.git.justcoin-lab <hello.git.justcoin@naver.com>
2025-02-13 16:04:59 +0900

    메인접근 금지

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("메인입니다");
         System.out.println("메인테스트");
         System.out.println("메인테스트");
+        System.out.println("메인테스트");
     }
 }
